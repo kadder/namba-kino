d36642a9952691e93afa3eca93ac796e4d638cef
Oleg Puzanov <puzanov@gmail.com>
2013-02-26 08:29:09 +0600

    Переделал дизайн

--- kino.rb
+++ kino.rb
@@ -3,28 +3,20 @@ require "bundler/setup"
 require "sinatra"
 require "sqlite3"
 
-set :environment, :production
-set :server, %w[thin]
+set :server, %w[webrick]
 set :port, 9494
 
 get '/' do
-  @sched = {}
   db = SQLite3::Database.new("Bishkek_Movie_Schedule_Builder/movie.sqlite3")
-  theaters = db.execute("SELECT DISTINCT theaterName FROM timeTable WHERE movieDate='2013-02-11'")
   time = Time.now.strftime("%H:%M")
   date = Time.now.strftime("%Y-%m-%d")
-  theaters.each do |t|
-    movies = db.execute("
-      SELECT movieName, movieTime, moviePrice, movieHall 
-      FROM timeTable 
-      WHERE 
-        movieDate='#{date}' AND 
-        movieTime >= '#{time}' AND
-        theaterName='#{t}'
-      ORDER BY movieTime ASC
-      LIMIT 0, 3
-      ")
-    @sched[t] = movies
-  end
+  puts db.inspect
+  @movies = db.execute("
+    SELECT theaterName, movieName, movieTime, moviePrice, movieHall
+    FROM timeTable
+    WHERE
+      movieDate='#{date}' AND
+      movieTime >= '#{time}'
+    ORDER BY movieTime ASC")
   erb :home
 end
